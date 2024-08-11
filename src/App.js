b13f3b5901abae923a168446f20dc77f7a57dade
PriyanshuGahlot <anshu2084gahlot@gmail.com>
2024-08-12 04:43:12 +0530

    poke api added and working

--- src/App.js
+++ src/App.js
@@ -5,18 +5,27 @@ import MainBtn from './components/MainBtn';
 
 function App() {
 
-  const [imgUrl, setimgUrl] = useState("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/132.png")
+  const [imgUrl, setimgUrl] = useState(null)
+  const [name, setname] = useState(null)
 
   const loadNextPokemon = () => {
-    
+    let x = Math.floor((Math.random() * 1025) + 1);
+    let req = fetch(`https://pokeapi.co/api/v2/pokemon/${x}`)
+    req.then((res) => {
+      return res.json()
+    }).then((data) => {
+      setimgUrl(data.sprites.other["official-artwork"].front_default)
+      setname(data.name[0].toUpperCase() + data.name.slice(1))
+    })
   }
 
   return (
     <>
     <p className='text-3xl text-center mt-10'>Pokémon</p>
     <p className='text-center text-2xl'>Smash Or Pass</p>
-    <div className='flex items-center justify-center'>
+    <div className='flex flex-col items-center justify-center'>
       <ImgHolder src = {imgUrl}/>
+      <h1 className='text-center text-2xl'>{name}</h1>
     </div>
     <div className='flex items-center justify-center gap-10 my-5'>
     <MainBtn txt = "Smash" onClick={loadNextPokemon}/>
--- src/components/ImgHolder.js
+++ src/components/ImgHolder.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ImgHolder(props) {
   return (
     props.src && <>
-      <img src={props.src} alt="Pokemon Image" className='w-1/5 h-1/5'/>
+    <img src={props.src} alt="Pokemon Image" className='w-1/5 h-1/5'/>
     </>
   )
 }
